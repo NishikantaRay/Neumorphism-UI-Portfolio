8ece06713b6a363bbc5322b0f1688f8253eecf37
Nishikantaray <nishikantaray1@gmail.com>
2021-07-19 19:29:15 +0530

    create nav and header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './home/nav/nav.component';
+import { HeaderComponent } from './home/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
