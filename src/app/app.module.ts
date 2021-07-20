a381cefec6544fc090922af3d29360cdb4757497
Nishikantaray <nishikantaray1@gmail.com>
2021-07-20 18:09:08 +0530

    added particle js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './home/nav/nav.component';
 import { HeaderComponent } from './home/header/header.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    HeaderComponent
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/home/header/header.component.ts
+++ src/app/home/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ParticlesConfig } from 'src/app/particles-config';
+declare let particlesJS: any;
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -10,6 +11,9 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.invokeParticles();
   }
-
+  public invokeParticles(): void {
+    particlesJS('particles-js', ParticlesConfig, function() {});
+  }
 }
