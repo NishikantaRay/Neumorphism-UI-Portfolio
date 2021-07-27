ddc1090b06d576320ee1ebd2c0dccc33448955c9
Nishikantaray <nishikantaray1@gmail.com>
2021-07-27 16:25:27 +0530

    update code

--- src/app/home/education/education.component.ts
+++ src/app/home/education/education.component.ts
@@ -9,7 +9,25 @@ import { Component, OnInit ,ViewEncapsulation } from '@angular/core';
 
 })
 export class EducationComponent implements OnInit {
+  eduarray:any =[
+    {
+      "name":"Silicon Institute of Technology,Bhubaneswer",
+      "year":"2019--2023"
+    },
+    {
+      "name":"Vidyarthee Residential College,Dhenkanal",
+      "year":"2017-2019"
+    },
+    {
+      "name":"Brajanath Badajena High School,Dhenkanal",
+      "year":"2013-2017"
+    },
+    {
+      "name":"Saraswati sishu Vidya Mandir,Dhenkanal",
+      "year":"2006-2013"
+    }
 
+  ]
   constructor() { }
 
   ngOnInit(): void {
