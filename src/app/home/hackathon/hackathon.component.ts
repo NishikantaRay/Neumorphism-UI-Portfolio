93c445e13532333bd762138fb4ab719013efa942
Nishikantaray <nishikantaray1@gmail.com>
2021-07-27 22:05:47 +0530

    update code

--- src/app/home/hackathon/hackathon.component.ts
+++ src/app/home/hackathon/hackathon.component.ts
@@ -11,5 +11,26 @@ export class HackathonComponent implements OnInit {
 
   ngOnInit(): void {
   }
+ arr:any=[{
+   "img":"https://raw.githubusercontent.com/NishikantaRay/instafeed/main/Screenshot%20(38).png?token=AO5W6YFZ5DIZMICWWFH3LZDBAVSWO",
+   "name":"BVP hackathon",
+   "desc":"Bharati Vidyapeeth's College of Engineering is organizing a Hackathon in its annual fest BVEST"
+ },
+ {
+  "img":"https://raw.githubusercontent.com/NishikantaRay/instafeed/main/Screenshot%20(38).png?token=AO5W6YFZ5DIZMICWWFH3LZDBAVSWO",
+  "name":"BVP hackathon",
+  "desc":"Bharati Vidyapeeth's College of Engineering is organizing a Hackathon in its annual fest BVEST"
+},
+{
+  "img":"https://raw.githubusercontent.com/NishikantaRay/instafeed/main/Screenshot%20(38).png?token=AO5W6YFZ5DIZMICWWFH3LZDBAVSWO",
+  "name":"BVP hackathon",
+  "desc":"Bharati Vidyapeeth's College of Engineering is organizing a Hackathon in its annual fest BVEST"
+},
+{
+  "img":"https://raw.githubusercontent.com/NishikantaRay/instafeed/main/Screenshot%20(38).png?token=AO5W6YFZ5DIZMICWWFH3LZDBAVSWO",
+  "name":"BVP hackathon",
+  "desc":"Bharati Vidyapeeth's College of Engineering is organizing a Hackathon in its annual fest BVEST"
+},
 
+]
 }
