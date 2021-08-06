c7a36efc6c6c19b554bf321d1ee91b00a4808b70
Nishikantaray <nishikantaray1@gmail.com>
2021-08-06 21:05:53 +0530

    Update hooby components

--- src/app/home/hobby/hobby.component.ts
+++ src/app/home/hobby/hobby.component.ts
@@ -6,7 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hobby.component.css']
 })
 export class HobbyComponent implements OnInit {
-
+  arrproject:any =[
+    {
+      "url":"https://raw.githubusercontent.com/NishikantaRay/Nishikantaray.github.io/main/sergio2.jpg",
+      "name":"Sergio"
+    },
+    {
+      "url":"https://raw.githubusercontent.com/NishikantaRay/Nishikantaray.github.io/main/sergio.jpg",
+      "name":"Sergio"
+    },
+    {
+      "url":"https://raw.githubusercontent.com/NishikantaRay/Nishikantaray.github.io/main/face.jpg",
+      "name":"Face Illustrator"
+    },
+    {
+      "url":"https://raw.githubusercontent.com/NishikantaRay/Nishikantaray.github.io/main/img/sanjay.jpeg",
+      "name":"Sanjay Dutt"
+    },
+    {
+      "url":"https://raw.githubusercontent.com/NishikantaRay/Nishikantaray.github.io/main/img/carry.jpeg",
+      "name":"CarryMinati"
+    },
+    {
+      "url":"https://raw.githubusercontent.com/NishikantaRay/Nishikantaray.github.io/main/img/cards.jpeg",
+      "name":"QueenofDecks"
+    }
+  ];
   constructor() { }
 
   ngOnInit(): void {
