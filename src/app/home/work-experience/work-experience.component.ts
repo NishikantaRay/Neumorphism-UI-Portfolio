1be2cdef032a0840fcd6a3d10245d1abce563a68
Nishikantaray <nishikantaray1@gmail.com>
2021-08-06 23:06:32 +0530

    Update components

--- src/app/home/work-experience/work-experience.component.ts
+++ src/app/home/work-experience/work-experience.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./work-experience.component.css']
 })
 export class WorkExperienceComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit(): void {
