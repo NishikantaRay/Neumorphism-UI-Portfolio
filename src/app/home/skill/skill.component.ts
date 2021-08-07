227873a8e5ca27202f311e300dc778b1c1efaac5
Nishikantaray <nishikantaray1@gmail.com>
2021-08-07 18:33:13 +0530

    Added Service for skills Component

--- src/app/home/skill/skill.component.ts
+++ src/app/home/skill/skill.component.ts
@@ -1,62 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import {SkillsServiceService} from '../../services/skills/skills-service.service'
 @Component({
   selector: 'app-skill',
   templateUrl: './skill.component.html',
   styleUrls: ['./skill.component.css']
 })
 export class SkillComponent implements OnInit {
-  arrproject:any =[
-    {
-      "url":"https://raw.githubusercontent.com/rahulbanerjee26/githubAboutMeGenerator/main/icons/javascript.svg",
-      "name":"JAVASCRIPT"
-    },
-    {
-      "url":"https://raw.githubusercontent.com/rahulbanerjee26/githubAboutMeGenerator/main/icons/angularjs.svg",
-      "name":"ANGULAR"
-    }
-    ,
-    {
-      "url":"https://raw.githubusercontent.com/rahulbanerjee26/githubAboutMeGenerator/main/icons/bootstrap.svg",
-      "name":"BOOTSTRAP"
-    }
-    ,
-    {
-      "url":"https://raw.githubusercontent.com/rahulbanerjee26/githubAboutMeGenerator/main/icons/github.svg",
-      "name":"GITHUB"
-    }
-    ,
-    {
-      "url":"https://raw.githubusercontent.com/rahulbanerjee26/githubAboutMeGenerator/main/icons/nodejs.svg",
-      "name":"NODEJS"
-    }
-    ,
-    {
-      "url":"https://raw.githubusercontent.com/rahulbanerjee26/githubAboutMeGenerator/main/icons/express.svg",
-      "name":"EXPRESS"
-    }
-    ,
-    {
-      "url":"https://raw.githubusercontent.com/rahulbanerjee26/githubAboutMeGenerator/main/icons/mongodb.svg",
-      "name":"MONGODB"
-    },
-    {
-      "url":"https://raw.githubusercontent.com/rahulbanerjee26/githubAboutMeGenerator/main/icons/c.svg",
-      "name":"C"
-    },
-    {
-      "url":"https://raw.githubusercontent.com/rahulbanerjee26/githubAboutMeGenerator/main/icons/html.svg",
-      "name":"HTML"
-    }
-    ,
-    {
-      "url":"https://raw.githubusercontent.com/rahulbanerjee26/githubAboutMeGenerator/main/icons/css.svg",
-      "name":"CSS"
-    }
-  ];
-  constructor() { }
-
+  
+  constructor(private SkillsServiceService:SkillsServiceService) { }
+  arrproject:any=[];
   ngOnInit(): void {
+    this.arrproject=this.SkillsServiceService.getData();
   }
 
 }
