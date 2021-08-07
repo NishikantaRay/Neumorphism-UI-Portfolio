426940e0abb7c569f0fec1cc73b4e0645f20549f
Nishikantaray <nishikantaray1@gmail.com>
2021-08-07 16:19:40 +0530

    Added Service for data rendering

--- src/app/home/achievements/achievements.component.ts
+++ src/app/home/achievements/achievements.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {AchieveService} from '../../services/achieve.service'
 @Component({
   selector: 'app-achievements',
   templateUrl: './achievements.component.html',
@@ -7,30 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AchievementsComponent implements OnInit {
   name: any = 'Achievements';
-  arrproject: any = [
-    {
-      img: 'https://raw.githubusercontent.com/NishikantaRay/Minimalist-portfolio/main/img/p1.png',
-      details:
-        ' The first, (currently only) and hands down BEST bootstrap 5 snippet extension. Includes templates, powerful utility snippets, and much more.',
-      name: 'Bootstrap 5 Code Snippets ',
-      link: 'https://marketplace.visualstudio.com/items?itemName=Nishikanta12.bootstrap5snippets',
-    },
-    {
-      img: 'https://raw.githubusercontent.com/NishikantaRay/Minimalist-portfolio/main/img/p2.png',
-      details:
-        ' StudyTub is an online platform where you can get all engineering study material ,video lectures and assignment solution',
-      name: 'Studytub ',
-      link: 'https://studytub.ml',
-    },
-    {
-      img: 'https://raw.githubusercontent.com/NishikantaRay/Minimalist-portfolio/main/img/p3.png',
-      details:
-        'To Control the black market of medicines, Control the production of Falsified drugs and provide real and trusted medicines to the buyer.',
-      name: 'Hackthe mountain 2.0',
-      link: 'https://devfolio.co/submissions/eliminating-black-market-of-medicines-0cd3',
-    },
-  ];
-  constructor() {}
+  
+  arrproject:any=[];
+  constructor(private AchieveService:AchieveService) {
+    
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.arrproject=this.AchieveService.getData();
+  }
 }
