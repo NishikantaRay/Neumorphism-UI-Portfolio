32ffc5fbd1762304b0a3eb501e7f249de04408ef
Nishikantaray <nishikantaray1@gmail.com>
2021-08-07 16:48:18 +0530

    Added Service for Hobby Component

--- src/app/home/hobby/hobby.component.ts
+++ src/app/home/hobby/hobby.component.ts
@@ -1,40 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import {HobbyServiceService} from '../../services/hobby/hobby-service.service';
 @Component({
   selector: 'app-hobby',
   templateUrl: './hobby.component.html',
   styleUrls: ['./hobby.component.css']
 })
 export class HobbyComponent implements OnInit {
-  arrproject:any =[
-    {
-      "url":"https://raw.githubusercontent.com/NishikantaRay/Nishikantaray.github.io/main/sergio2.jpg",
-      "name":"Sergio"
-    },
-    {
-      "url":"https://raw.githubusercontent.com/NishikantaRay/Nishikantaray.github.io/main/sergio.jpg",
-      "name":"Sergio"
-    },
-    {
-      "url":"https://raw.githubusercontent.com/NishikantaRay/Nishikantaray.github.io/main/face.jpg",
-      "name":"Face Illustrator"
-    },
-    {
-      "url":"https://raw.githubusercontent.com/NishikantaRay/Nishikantaray.github.io/main/img/sanjay.jpeg",
-      "name":"Sanjay Dutt"
-    },
-    {
-      "url":"https://raw.githubusercontent.com/NishikantaRay/Nishikantaray.github.io/main/img/carry.jpeg",
-      "name":"CarryMinati"
-    },
-    {
-      "url":"https://raw.githubusercontent.com/NishikantaRay/Nishikantaray.github.io/main/img/cards.jpeg",
-      "name":"QueenofDecks"
-    }
-  ];
-  constructor() { }
-
+  
+  constructor(private HobbyServiceService :HobbyServiceService) { }
+  arrproject:any =[];
   ngOnInit(): void {
+    this.arrproject=this.HobbyServiceService.getData();
   }
 
 }
